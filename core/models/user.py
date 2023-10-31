f2854f3d03386cee4a1b009e1b16c0845f96e314
PyPack <serg.karetnuk333@gmail.com>
2023-10-31 09:40:16 +0100

    add relation posts -> user

--- core/models/user.py
+++ core/models/user.py
@@ -1,8 +1,14 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .post import Post
 
 
 class User(Base):
     user_name: Mapped[str] = mapped_column(String(32), unique=True)
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
