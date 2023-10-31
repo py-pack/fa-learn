1c841415b2d60624ea8d4396100ef443fbec42d1
PyPack <serg.karetnuk333@gmail.com>
2023-10-31 20:03:26 +0100

    add profile table

---
+++ core/models/profile.py
@@ -0,0 +1,19 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+from .mixins import UserRelationMixin
+
+if TYPE_CHECKING:
+    from .user import User
+
+
+class Profile(UserRelationMixin, Base):
+    _user_id_unique = True
+    _user_back_populate = "profile"
+
+    first_name: Mapped[str | None] = mapped_column(String(40))
+    last_name: Mapped[str | None] = mapped_column(String(40))
+    bio: Mapped[str | None] = mapped_column(String)
