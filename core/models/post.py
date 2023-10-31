5bf1708ae147853bc5b4106bf5f6621f7bcfce0d
PyPack <serg.karetnuk333@gmail.com>
2023-10-31 20:03:13 +0100

    add user relation mixin

--- core/models/post.py
+++ core/models/post.py
@@ -1,23 +1,20 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
+from .mixins import UserRelationMixin
 
 if TYPE_CHECKING:
     from .user import User
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    _user_back_populate = "posts"
     title: Mapped[str] = mapped_column(String(100))
     body: Mapped[str] = mapped_column(
         Text,
         default="",
         server_default="",
     )
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-        # nullable=False
-    )
-    user: Mapped["User"] = relationship(back_populates="posts")
